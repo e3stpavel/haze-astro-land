ab756cef68af17e0ad57a68bccbe5eabf9ac912f
eestpavel <pavel@nexd.com>
2022-09-05 13:05:04 +0300

    FEAT(dev): added multiple styles support, enhanced Button component styles variations

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,8 +1,8 @@
 export { default as Button } from './Button.astro'
 
 // styles configurations
-export { default as Default } from './styles/default.module.scss'
 export { default as Rounded } from './styles/rounded.module.scss'
 export { default as Edgy } from './styles/edgy.module.scss'
 export { default as Outlined } from './styles/outlined.module.scss'
 export { default as Ghost } from './styles/ghost.module.scss'
+export { default as Link } from './styles/link.module.scss'
